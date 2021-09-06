419ce35d5e1235b1e5fe1a3c7dd2e4dda44dc91e
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-09-06 11:38:44 +0100

    m

--- adaptive_retrieval/divide_dev_set.py
+++ adaptive_retrieval/divide_dev_set.py
@@ -23,7 +23,7 @@ data_size = len(lines_de)
 train_size = int(.9*data_size)
 dev_size = data_size-train_size
 
-train_lines = np.random.randint(data_size,size=train_size)[0].tolist()
+train_lines = np.random.randint(data_size,size=train_size).tolist()
 
 print(train_lines)
 
