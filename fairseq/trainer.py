180da9fb87a5ab53b52a865b666569f8120bd366
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-09-15 16:36:48 +0100

    m

--- fairseq/trainer.py
+++ fairseq/trainer.py
@@ -270,7 +270,7 @@ class Trainer(object):
             )
         )
         print('ppppppppppppppppppppppppppppppp')
-        print([n, p for n,p in self.model.named_parameters()])
+        print([p[0] for p in self.model.named_parameters()])
         if self.is_fsdp and self.cfg.common.fp16:
             # FullyShardedDataParallel always uses MemoryEfficientFP16 wrapper,
             # mostly for the grad scaling. But if we don't have the
