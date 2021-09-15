5d089a5e1a5cd19316bdc1e14df62f6a99027831
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-09-15 15:32:15 +0100

    m

--- fairseq/models/transformer/transformer_base.py
+++ fairseq/models/transformer/transformer_base.py
@@ -99,6 +99,8 @@ class TransformerModelBase(FairseqEncoderDecoderModel):
 
             for name, param in decoder.named_parameters():
                 if "lambda" in name and cfg.knn_lambda_type == "trainable":
+                    print('............................................')
+                    print(name)
                     param.requires_grad = True
 
         return cls(cfg, encoder, decoder)
