7080114c68ceea3bb8ab0cf968d0802281a6c805
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-11-28 00:48:21 +0100

    m

--- datastore_pruning/datastore_pruning.py
+++ datastore_pruning/datastore_pruning.py
@@ -27,10 +27,10 @@ res = faiss.StandardGpuResources()
 if args.dstore_fp16:
     print('load dstore fp16', args.dstore_size, args.dimension)
     keys = np.memmap(args.dstore_mmap + '/keys.npy', dtype=np.float16, mode='r', shape=(args.dstore_size, args.dimension))
-    vals = np.memmap(args.dstore_mmap + '/vals.npy', dtype=np.int, mode='r', shape=(args.dstore_size, 1))
+    vals = np.memmap(args.dstore_mmap + '/vals.npy', dtype=int, mode='r', shape=(args.dstore_size, 1))
 else:
     keys = np.memmap(args.dstore_mmap + '/keys.npy', dtype=np.float32, mode='r', shape=(args.dstore_size, args.dimension))
-    vals = np.memmap(args.dstore_mmap + '/vals.npy', dtype=np.int, mode='r', shape=(args.dstore_size, 1))
+    vals = np.memmap(args.dstore_mmap + '/vals.npy', dtype=int, mode='r', shape=(args.dstore_size, 1))
 
 def parse_retrieve_fname(fname):
     offset = size = nk = None
@@ -51,7 +51,7 @@ def parse_retrieve_fname(fname):
 
     raise ValueError(f"parsing error for {fname}")
 
-weights = np.ones(args.dstore_size, dtype=np.int)
+weights = np.ones(args.dstore_size, dtype=int)
 
 def merge_knn(fname):
     print(f'start processing {fname}', flush=True)
@@ -59,10 +59,10 @@ def merge_knn(fname):
     print(f'offset: {offset}, size{size}, k{nk}', flush=True)
     scores = np.zeros(args.dstore_size, dtype=np.float32)
 
-    ret = np.memmap(os.path.join(args.retrieval_dir, fname), dtype=np.int32, mode='r', shape=(size, nk))
+    ret = np.memmap(os.path.join(args.retrieval_dir, fname), dtype=int32, mode='r', shape=(size, nk))
 
     t = time.time()
-    ret_mem = np.zeros((size, args.k+1), dtype=np.int32)
+    ret_mem = np.zeros((size, args.k+1), dtype=int32)
     ret_mem[:] = ret[:, :args.k+1]
     print(f'reading index into memory costs {time.time() - t} seconds', flush=True)
 
@@ -113,16 +113,16 @@ if args.dstore_fp16:
     new_key = np.memmap(os.path.join(args.save_dir,
             f'dstore_merge{args.k}_size{num}_keys.npy'), mode='w+', dtype=np.float16, shape=(num, args.dimension))
     new_val = np.memmap(os.path.join(args.save_dir,
-            f'dstore_merge{args.k}_size{num}_vals.npy'), mode='w+', dtype=np.int, shape=(num, 1))
+            f'dstore_merge{args.k}_size{num}_vals.npy'), mode='w+', dtype=int, shape=(num, 1))
     new_weight = np.memmap(os.path.join(args.save_dir,
-            f'dstore_merge{args.k}_size{num}_weights.npy'), mode='w+', dtype=np.int, shape=(num, 1))
+            f'dstore_merge{args.k}_size{num}_weights.npy'), mode='w+', dtype=int, shape=(num, 1))
 else:
     new_key = np.memmap(os.path.join(args.save_dir,
             f'dstore_merge{args.k}_size{num}_keys.npy'), mode='w+', dtype=np.float32, shape=(num, args.dimension))
     new_val = np.memmap(os.path.join(args.save_dir,
-            f'dstore_merge{args.k}_size{num}_vals.npy'), mode='w+', dtype=np.int, shape=(num, 1))
+            f'dstore_merge{args.k}_size{num}_vals.npy'), mode='w+', dtype=int, shape=(num, 1))
     new_weight = np.memmap(os.path.join(args.save_dir,
-            f'dstore_merge{args.k}_size{num}_weights.npy'), mode='w+', dtype=np.int, shape=(num, 1))
+            f'dstore_merge{args.k}_size{num}_weights.npy'), mode='w+', dtype=int, shape=(num, 1))
 
 cnt = 0
 for i, v in enumerate(weights):
