563e5225f014e2d39f21e6c591acf4fcf4d8a6ea
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-01 13:08:41 +0100

    m

--- generate_knnmt.py
+++ generate_knnmt.py
@@ -249,9 +249,8 @@ def _main(cfg: DictConfig, output_file):
                     remove_bpe=cfg.common_eval.post_process,
                     extra_symbols_to_ignore=get_symbols_to_strip_from_output(generator),)
 
-                print('hypo',hypo['tokens'])
-
                 detok_hypo_str = decode_fn(hypo_str)
+                print(detok_hypo_str)
                 if not cfg.common_eval.quiet:
                     score = hypo["score"] / math.log(2)  # convert to base 2
                     # original hypothesis (after tokenization and BPE)
