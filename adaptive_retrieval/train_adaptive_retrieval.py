7c91e3034362b755991beca564a37b815ccf68c1
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-06 10:53:57 +0100

    m

--- adaptive_retrieval/train_adaptive_retrieval.py
+++ adaptive_retrieval/train_adaptive_retrieval.py
@@ -52,6 +52,10 @@ def validate(val_dataloader, model, args):
         
         cross_entropy = log_weight + torch.stack((torch.log(network_prob), torch.log(knn_prob)), dim=-1)
 
+        print('log_weight', log_weight)
+        print('network_prob', network_prob)
+        print('knn_prob', knn_prob)
+
         # (B,)
         cross_entropy = -torch.logsumexp(cross_entropy, dim=-1)
         loss = cross_entropy.mean()
