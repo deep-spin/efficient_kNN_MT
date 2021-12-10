ba3ca79567afb9e8d56919b41287b3b6f254aedb
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-10 10:24:31 +0100

    m

--- save_adaptive_retrieval_features.py
+++ save_adaptive_retrieval_features.py
@@ -78,14 +78,14 @@ def main(args, override_args=None):
         progress = progress_bar.progress_bar(itr, log_format=args.log_format, log_interval=args.log_interval,
            prefix=f"valid on '{subset}' subset", default_log_format=("tqdm" if not args.no_progress_bar else "simple"),)
 
-        targets_file = np.memmap(override_args.adaptive_retrieval_features_path+'_targets', 
-                            dtype='int', mode='w+', shape=(override_args.adaptive_retrieval_features_size,1))
-        features_file = np.memmap(override_args.adaptive_retrieval_features_path+'_features', 
-                            dtype='float32', mode='w+', shape=(override_args.adaptive_retrieval_features_size,1024))
-        knn_probs_file = np.memmap(override_args.adaptive_retrieval_features_path+'_knn_probs', 
-                            dtype='float32', mode='w+', shape=(override_args.adaptive_retrieval_features_size,42024))
-        network_probs_file = np.memmap(override_args.adaptive_retrieval_features_path+'_network_probs', 
-                            dtype='float32', mode='w+', shape=(override_args.adaptive_retrieval_features_size,42024))
+        #targets_file = np.memmap(override_args.adaptive_retrieval_features_path+'_targets', 
+        #                    dtype='int', mode='w+', shape=(override_args.adaptive_retrieval_features_size,1))
+        #features_file = np.memmap(override_args.adaptive_retrieval_features_path+'_features', 
+        #                    dtype='float32', mode='w+', shape=(override_args.adaptive_retrieval_features_size,1024))
+        #knn_probs_file = np.memmap(override_args.adaptive_retrieval_features_path+'_knn_probs', 
+        #                    dtype='float32', mode='w+', shape=(override_args.adaptive_retrieval_features_size,42024))
+        #network_probs_file = np.memmap(override_args.adaptive_retrieval_features_path+'_network_probs', 
+        #                    dtype='float32', mode='w+', shape=(override_args.adaptive_retrieval_features_size,42024))
 
         aux=0
        	with torch.no_grad():
