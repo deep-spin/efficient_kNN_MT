cca5585d405ac79e5b690b25d744c5abd511df9f
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-12 16:38:52 +0100

    m

--- adaptive_retrieval/train_adaptive_retrieval.py
+++ adaptive_retrieval/train_adaptive_retrieval.py
@@ -16,39 +16,41 @@ from collections import Counter, OrderedDict
 from lambda_mlp import LambdaMLP
 
 
-#class FeatureDataset(data.Dataset):
-#    def __init__(self, data):
-#        self.features = data['features']
-#        self.targets = data['targets']
-#        self.knn_probs = data['knn_probs']
-#        self.network_probs = data['network_probs']
-
-#    def __len__(self):
-#        return len(self.features)
-
-#    def __getitem__(self, idx):
-#        return self.features[idx].cuda(), self.targets[idx].cuda(), self.knn_probs[idx].cuda(), self.network_probs[idx].cuda()
-
 class FeatureDataset(data.Dataset):
-    def __init__(self, targets, features, knn_probs, network_probs):
-        self.targets=targets
-        self.features=features
-        self.knn_probs=knn_probs
-        self.network_probs=network_probs
+    def __init__(self, data):
+        self.features = data['features']
+        self.targets = data['targets']
+        self.knn_probs = data['knn_probs']
+        self.network_probs = data['network_probs']
+        self.conf = data['conf']
+        self.ent = data['ent']
 
     def __len__(self):
-        return len(self.targets)
+        return len(self.features)
 
     def __getitem__(self, idx):
+        return self.features[idx].cuda(), self.targets[idx].cuda(), self.knn_probs[idx].cuda(), self.network_probs[idx].cuda(), self.conf[idx].cuda(), self.ent[idx].cuda()
+
+#class FeatureDataset(data.Dataset):
+#    def __init__(self, targets, features, knn_probs, network_probs):
+#        self.targets=targets
+#        self.features=features
+#        self.knn_probs=knn_probs
+#        self.network_probs=network_probs
+
+#    def __len__(self):
+#        return len(self.targets)
+
+#    def __getitem__(self, idx):
         #return torch.FloatTensor(self.features[idx]).cuda(), self.targets[idx].cuda(), torch.FloatTensor(self.knn_probs[idx]).cuda(), torch.FloatTensor(self.network_probs[idx]).cuda()
-        return self.features[idx].cuda(), self.targets[idx].cuda(), self.knn_probs[idx].cuda(), self.network_probs[idx].cuda()
+#        return self.features[idx].cuda(), self.targets[idx].cuda(), self.knn_probs[idx].cuda(), self.network_probs[idx].cuda()
 
 def validate(val_dataloader, model, args):
     model.eval()
     running_loss = 0.
     nsamples = 0
     for i, sample in enumerate(val_dataloader):
-        features, targets, knn_probs, network_probs = sample[0], sample[1], sample[2], sample[3]
+        features, targets, knn_probs, network_probs, conf, ent = sample[0], sample[1], sample[2], sample[3], sample[4], sample[5]
 
         for v in range(len(targets)):
         	if v==0:
@@ -64,8 +66,8 @@ def validate(val_dataloader, model, args):
         if not args.use_conf_ent:
         	log_weight = model(features)
         else:
-        	conf=torch.max(network_probs, -1).values.unsqueeze(-1)
-        	ent=torch.distributions.Categorical(network_probs).entropy().unsqueeze(-1)
+        	#conf=torch.max(network_probs, -1).values.unsqueeze(-1)
+        	#ent=torch.distributions.Categorical(network_probs).entropy().unsqueeze(-1)
         	log_weight = model(features, conf, ent)
         
         #log_weight = torch.log(torch.FloatTensor([.4,.6])).cuda().unsqueeze(0)
@@ -129,33 +131,33 @@ print(args)
 np.random.seed(args.seed)
 torch.manual_seed(args.seed)
 
-#train_data = torch.load(args.train_file)
-#valid_data = torch.load(args.val_file)
+train_data = torch.load(args.train_file)
+valid_data = torch.load(args.val_file)
 
-targets_file = torch.load(args.train_file+'_targets')
-features_file = np.memmap(args.train_file+'_features', dtype='float32', mode='r+', shape=(targets_file.size(0), 1024))
-knn_probs_file = np.memmap(args.train_file+'_knn_probs', dtype='float32', mode='r+', shape=(targets_file.size(0), 42024))
-network_probs_file = np.memmap(args.train_file+'_network_probs', dtype='float32', mode='r+', shape=(targets_file.size(0), 42024))
+#targets_file = torch.load(args.train_file+'_targets')
+#features_file = np.memmap(args.train_file+'_features', dtype='float32', mode='r+', shape=(targets_file.size(0), 1024))
+#knn_probs_file = np.memmap(args.train_file+'_knn_probs', dtype='float32', mode='r+', shape=(targets_file.size(0), 42024))
+#network_probs_file = np.memmap(args.train_file+'_network_probs', dtype='float32', mode='r+', shape=(targets_file.size(0), 42024))
 
-targets_val_file = torch.load(args.val_file+'_targets')
-features_val_file = np.memmap(args.val_file+'_features', dtype='float32', mode='r+', shape=(targets_val_file.size(0), 1024))
-knn_probs_val_file = np.memmap(args.val_file+'_knn_probs', dtype='float32', mode='r+', shape=(targets_val_file.size(0), 42024))
-network_probs_val_file = np.memmap(args.val_file+'_network_probs', dtype='float32', mode='r+', shape=(targets_val_file.size(0), 42024))
+#targets_val_file = torch.load(args.val_file+'_targets')
+#features_val_file = np.memmap(args.val_file+'_features', dtype='float32', mode='r+', shape=(targets_val_file.size(0), 1024))
+#knn_probs_val_file = np.memmap(args.val_file+'_knn_probs', dtype='float32', mode='r+', shape=(targets_val_file.size(0), 42024))
+#network_probs_val_file = np.memmap(args.val_file+'_network_probs', dtype='float32', mode='r+', shape=(targets_val_file.size(0), 42024))
 
-features_file=torch.from_numpy(features_file)
-knn_probs_file=torch.from_numpy(knn_probs_file)
-network_probs_file=torch.from_numpy(network_probs_file)
+#features_file=torch.from_numpy(features_file)
+#knn_probs_file=torch.from_numpy(knn_probs_file)
+#network_probs_file=torch.from_numpy(network_probs_file)
 
-features_val_file=torch.from_numpy(features_val_file)
-knn_probs_val_file=torch.from_numpy(knn_probs_val_file)
-network_probs_val_file=torch.from_numpy(network_probs_val_file)
+#features_val_file=torch.from_numpy(features_val_file)
+#knn_probs_val_file=torch.from_numpy(knn_probs_val_file)
+#network_probs_val_file=torch.from_numpy(network_probs_val_file)
 
 
-#training_set = FeatureDataset(train_data)
-#val_set = FeatureDataset(valid_data)
+training_set = FeatureDataset(train_data)
+val_set = FeatureDataset(valid_data)
 
-training_set = FeatureDataset(targets_file, features_file, knn_probs_file, network_probs_file)
-val_set = FeatureDataset(targets_val_file, features_val_file, knn_probs_val_file, network_probs_val_file)
+#training_set = FeatureDataset(targets_file, features_file, knn_probs_file, network_probs_file)
+#val_set = FeatureDataset(targets_val_file, features_val_file, knn_probs_val_file, network_probs_val_file)
 
 train_dataloader = torch.utils.data.DataLoader(training_set,
                                                batch_size=args.batch_size,
@@ -194,7 +196,7 @@ for epoch in tqdm(range(args.n_epochs)):
     nsamples = 0
 
     for i, sample in enumerate(tqdm(train_dataloader)):
-        features, targets, knn_probs, network_probs = sample[0], sample[1], sample[2], sample[3]
+        features, targets, knn_probs, network_probs, conf, ent = sample[0], sample[1], sample[2], sample[3], sample[4], sample[5]
 
         for v in range(len(targets)):
         	if v==0:
@@ -211,8 +213,8 @@ for epoch in tqdm(range(args.n_epochs)):
         if not args.use_conf_ent:
         	log_weight = model(features)
         else:
-        	conf=torch.max(network_probs, -1).values.unsqueeze(-1)
-        	ent=torch.distributions.Categorical(network_probs).entropy().unsqueeze(-1)
+        	#conf=torch.max(network_probs, -1).values.unsqueeze(-1)
+        	#ent=torch.distributions.Categorical(network_probs).entropy().unsqueeze(-1)
         	log_weight = model(features, conf, ent)
 
         cross_entropy = log_weight + torch.stack((torch.log(network_prob), torch.log(knn_prob)), dim=-1)
