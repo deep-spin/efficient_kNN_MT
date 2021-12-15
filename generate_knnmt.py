8f5e3caa522aa49ca8d061cc1f2e682147378f78
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-15 11:34:08 +0100

    m

--- generate_knnmt.py
+++ generate_knnmt.py
@@ -225,7 +225,7 @@ def _main(cfg: DictConfig, output_file):
             conf.append(torch.max(network_probs_difs, -1).values.cpu().data)
             ent.append(torch.distributions.Categorical(network_probs_difs).entropy().cpu().data)
 
-            print(knn_probs_difs.sum(-1))
+            print(knn_probs.sum())
 
         else:
             hypos = task.inference_step(
