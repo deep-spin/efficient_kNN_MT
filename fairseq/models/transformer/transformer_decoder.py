3c45d298b4fa600133491c0a8aa70722f3a1ff21
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-16 15:01:50 +0100

    m

--- fairseq/models/transformer/transformer_decoder.py
+++ fairseq/models/transformer/transformer_decoder.py
@@ -312,7 +312,9 @@ class TransformerDecoderBase(FairseqIncrementalDecoder):
                 self.oracle_mlp.eval()
 
                 scores = self.oracle_mlp.forward(last_hidden).squeeze(-1)
+                print(scores)
                 indices = (scores > 0.5).nonzero()[:,0]
+                print(indices)
                 mask = torch.ones(last_hidden.size(0), dtype=torch.bool)
                 mask[indices] = False
                 last_hidden=last_hidden[mask]
