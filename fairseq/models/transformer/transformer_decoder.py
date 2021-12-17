44c5d949df510941ba05745ff80f8d230fab66d9
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-17 10:28:22 +0000

    m

--- fairseq/models/transformer/transformer_decoder.py
+++ fairseq/models/transformer/transformer_decoder.py
@@ -342,7 +342,7 @@ class TransformerDecoderBase(FairseqIncrementalDecoder):
                 self.need_to_search += scores.size(0) - indices.size(0)
                 self.total_possible_searches+=scores.size(0)
 
-                print(self.need_to_search, self.total_possible_searches)
+                #print(self.need_to_search, self.total_possible_searches)
 
             if (self.knn_lambda_threshold == 0 and not self.knn_search_prediction) or last_hidden.size(0) > 0:
                 knn_search_result = self.knn_datastore.retrieve(last_hidden)
