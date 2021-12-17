08bb69c1f0ab63b9b1755c0c83a7bbfeaf6c7a36
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-17 12:14:54 +0100

    m

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -868,13 +868,13 @@ class EnsembleModel(nn.Module):
                 encoder_out = encoder_outs[i]
             # decode each model
             if self.has_incremental_states():
+                print('.....................', new_sent)
                 decoder_out = model.decoder.forward(
                     tokens,
                     encoder_out=encoder_out,
                     incremental_state=incremental_states[i],
                 )
             else:
-                print('.....................', new_sent)
                 if hasattr(model, "decoder"):
                     decoder_out = model.decoder.forward(tokens, new_sent, encoder_out=encoder_out)
                 else:
