34b8a4d9522a109a1dc3e1dcd05d6e91762d0825
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-22 17:26:33 +0000

    m

--- adaptive_retrieval/mlp_oracle.py
+++ adaptive_retrieval/mlp_oracle.py
@@ -85,10 +85,7 @@ class MLPOracle(nn.Module):
             features_cat.append(self.input_layer['fert_4'](fert_4))
             features_cat = torch.cat(features_cat,-1)
 
-            torch.set_printoptions(profile="full")
-            print(features_cat)
-            torch.set_printoptions(profile="default") # reset
-
+            print(features_cat.shape)
 
             scores = self.model(features_cat)
         else:
