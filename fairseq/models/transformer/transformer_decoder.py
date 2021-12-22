de13bbe9efafe842ad5277bd452dacd2da20aabd
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-22 13:19:46 +0000

    m

--- fairseq/models/transformer/transformer_decoder.py
+++ fairseq/models/transformer/transformer_decoder.py
@@ -413,6 +413,7 @@ class TransformerDecoderBase(FairseqIncrementalDecoder):
                 if self.knn_lambda_threshold > 0 or self.knn_search_prediction or self.use_knn_cache or self.use_faiss_centroids:    
                     knn_probs[mask]=knn_prob
                     if self.use_knn_cache:
+                        print(knn_prob.shape)
                         if self.knn_cache_probs is None:
                             self.knn_cache_probs=knn_prob
                         else:
