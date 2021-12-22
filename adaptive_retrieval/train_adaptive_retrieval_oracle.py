386b2abcd11b77fe744eb44afdf7733fc7c62c23
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-22 16:57:03 +0000

    m

--- adaptive_retrieval/train_adaptive_retrieval_oracle.py
+++ adaptive_retrieval/train_adaptive_retrieval_oracle.py
@@ -41,8 +41,6 @@ class FeatureDataset(data.Dataset):
         
         self.tokens=[]
         for sent in tokens:
-            print('\n\n')
-            print(sent)
             for i in range(len(sent)):
                 if i==0:
                     self.tokens.append((2,2,2,sent[i].item()))
@@ -53,7 +51,8 @@ class FeatureDataset(data.Dataset):
                 else:
                     self.tokens.append((sent[i-3].item(),sent[i-2].item(),sent[i-1].item(),sent[i].item()))
 
-                print(self.tokens)
+        print(len(self.tokens))
+        print(len(self.ent))
             
 
     def __len__(self):
