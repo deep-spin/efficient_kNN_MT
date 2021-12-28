a341999acc2f26c1a679882134083af0df8177f4
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-28 16:30:45 +0000

    m

--- adaptive_retrieval/train_adaptive_retrieval.py
+++ adaptive_retrieval/train_adaptive_retrieval.py
@@ -59,10 +59,6 @@ class FeatureDataset(data.Dataset):
             fert_3=torch.FloatTensor([self.fert_dict[self.tokens[idx][:-3]]])
             fert_4=torch.FloatTensor([self.fert_dict[self.tokens[idx][:-4]]])
 
-            print(freq_1.shape)
-            print(self.conf[idx].unsqueeze(-1).shape)
-
-
             return self.features[idx].cuda(), self.targets[idx].cuda(), self.knn_probs[idx].cuda(), self.network_probs[idx].cuda(), self.conf[idx].cuda().unsqueeze(-1), self.ent[idx].cuda().unsqueeze(-1), freq_1.cuda(), freq_2.cuda(), freq_3.cuda(), freq_4.cuda(), fert_1.cuda(), fert_2.cuda(), fert_3.cuda(), fert_4.cuda()
 
         elif self.use_faiss_centroids:
