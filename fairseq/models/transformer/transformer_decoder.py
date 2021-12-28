0b469e934724bbdbd24e45642da1b6b888033e52
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-28 02:21:25 +0000

    m

--- fairseq/models/transformer/transformer_decoder.py
+++ fairseq/models/transformer/transformer_decoder.py
@@ -434,6 +434,8 @@ class TransformerDecoderBase(FairseqIncrementalDecoder):
                     min_dist = dists.min(-1).values.unsqueeze(-1)
                     min_top32_dist = torch.topk(dists, 32, largest=False, dim=-1).values.unsqueeze(-1)
 
+                    print(min_dist.shape)
+                    print(min_top32_dist.shape)
                     scores = self.oracle_mlp.forward(last_hidden, conf=conf, ent=ent, min_dist=min_dist, min_top32_dist=min_top32_dist).squeeze(-1)                    
 
                 else:
