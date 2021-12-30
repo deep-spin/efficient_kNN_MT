212a12ff7b4c12737aa23bda3a4a2272e9f7b2d9
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-30 19:31:58 +0000

    m

--- datastore_splitting/split_train_datastore.py
+++ datastore_splitting/split_train_datastore.py
@@ -69,7 +69,7 @@ _, I = kmeans.index.search(keys, 1)
 
 print(kmeans.centroids)
 print(kmeans.centroids.shape)
-torch.save(kmeans.centroids, args.faiss_index+'centroids')
+np.save(kmeans.centroids, args.faiss_index+'centroids')
 
 log_file = open(args.faiss_index+'log', 'w')
 if not os.path.exists(args.faiss_index + "0_faiss_index.trained"):
