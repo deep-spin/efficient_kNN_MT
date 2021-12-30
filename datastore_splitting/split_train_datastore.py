89bf06a36ee4cc7d905dc03ce5ce9b0dd6e47cb3
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-30 23:19:28 +0000

    m

--- datastore_splitting/split_train_datastore.py
+++ datastore_splitting/split_train_datastore.py
@@ -61,9 +61,9 @@ index_dim = args.pca if args.pca > 0 else args.dimension
 
 
 random_sample = np.random.choice(np.arange(vals.shape[0]), size=[min(args.n_examples_train_kmeans, vals.shape[0])], replace=False)
-kmeans = faiss.Kmeans(index_dim, args.n_datastores, niter=args.kmeans_iter, verbose=True, min_points_per_centroid=1024)
+kmeans = faiss.Kmeans(index_dim, args.n_datastores, niter=args.kmeans_iter, verbose=True, min_points_per_centroid=10000)
 
-kmeans.train(keys[random_sample].astype(np.float32), min_points_per_centroid=1024)
+kmeans.train(keys[random_sample].astype(np.float32))
 
 _, I = kmeans.index.search(keys, 1)
 
