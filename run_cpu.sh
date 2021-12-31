e761a494d008fd03977e186b218ab844aae540b5
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-31 18:36:45 +0000

    m

--- run_cpu.sh
+++ run_cpu.sh
@@ -80,7 +80,6 @@ echo "koran pca 256 pruned 2 cache 6 batch 1"
 
 
 
-
 CUDA_VISIBLE_DEVICES=-1 python3 generate_knnmt.py /media/hdd1/pam/mt/data/joint_multi_domain/medical/data-bin/ --path /media/hdd1/pam/mt/models/wmt19.de-en/wmt19.de-en.pt --arch transformer_wmt19_de_en_with_datastore --gen-subset=test --beam 5 --batch-size=8 --source-lang de --target-lang en --scoring sacrebleu --max-tokens 4096 --tokenizer moses --remove-bpe --model-overrides "{'load_knn_datastore': True, 'use_knn_datastore': True, 'dstore_filename': '/media/hdd1/pam/mt/data/joint_multi_domain/medical/datastore_32/pruned/pca/pruned_2_pca_256_', 'dstore_size': 4039432, 'dstore_fp16': False, 'k': 8, 'probe': 32, 'faiss_metric_type': 'l2', 'use_gpu_to_search': False, 'move_dstore_to_mem': True, 'no_load_keys': True,'knn_lambda_type': 'fix', 'knn_lambda_value': .7, 'knn_temperature_type': 'fix', 'knn_temperature_value': 10, 'knn_cache': True, 'knn_cache_threshold': 6}" --quiet
 
 echo "medical pca 256 pruned 2 cache 6 batch 8"
@@ -96,6 +95,7 @@ echo "it pca 256 pruned 2 cache 6 batch 8"
 CUDA_VISIBLE_DEVICES=-1 python3 generate_knnmt.py /media/hdd1/pam/mt/data/joint_multi_domain/koran/data-bin/ --path /media/hdd1/pam/mt/models/wmt19.de-en/wmt19.de-en.pt --arch transformer_wmt19_de_en_with_datastore --gen-subset=test --beam 5 --batch-size=8 --source-lang de --target-lang en --scoring sacrebleu --max-tokens 4096 --tokenizer moses --remove-bpe --model-overrides "{'load_knn_datastore': True, 'use_knn_datastore': True, 'dstore_filename': '/media/hdd1/pam/mt/data/joint_multi_domain/koran/datastore_32/pruned/pca/pruned_2_pca_256_', 'dstore_size': 353007, 'dstore_fp16': False, 'k': 8, 'probe': 32, 'faiss_metric_type': 'l2', 'use_gpu_to_search': False, 'move_dstore_to_mem': True, 'no_load_keys': True,'knn_lambda_type': 'fix', 'knn_lambda_value': .7, 'knn_temperature_type': 'fix', 'knn_temperature_value': 100, 'knn_cache': True , 'knn_cache_threshold': 6}" --quiet
 
 echo "koran pca 256 pruned 2 cache 6 batch 8"
+
 
 
 
@@ -120,7 +120,6 @@ echo "koran pca 256 pruned 2 batch 1"
 
 
 
-
 CUDA_VISIBLE_DEVICES=-1 python3 generate_knnmt.py /media/hdd1/pam/mt/data/joint_multi_domain/medical/data-bin/ --path /media/hdd1/pam/mt/models/wmt19.de-en/wmt19.de-en.pt --arch transformer_wmt19_de_en_with_datastore --gen-subset=test --beam 5 --batch-size=8 --source-lang de --target-lang en --scoring sacrebleu --max-tokens 4096 --tokenizer moses --remove-bpe --model-overrides "{'load_knn_datastore': True, 'use_knn_datastore': True, 'dstore_filename': '/media/hdd1/pam/mt/data/joint_multi_domain/medical/datastore_32/pruned/pca/pruned_2_pca_256_', 'dstore_size': 4039432, 'dstore_fp16': False, 'k': 8, 'probe': 32, 'faiss_metric_type': 'l2', 'use_gpu_to_search': False, 'move_dstore_to_mem': True, 'no_load_keys': True,'knn_lambda_type': 'fix', 'knn_lambda_value': .7, 'knn_temperature_type': 'fix', 'knn_temperature_value': 10, 'knn_cache': False, 'knn_cache_threshold': 6}" --quiet
 
 echo "medical pca 256 pruned 2 batch 8"
@@ -136,6 +135,8 @@ echo "it pca 256 pruned 2 batch 8"
 CUDA_VISIBLE_DEVICES=-1 python3 generate_knnmt.py /media/hdd1/pam/mt/data/joint_multi_domain/koran/data-bin/ --path /media/hdd1/pam/mt/models/wmt19.de-en/wmt19.de-en.pt --arch transformer_wmt19_de_en_with_datastore --gen-subset=test --beam 5 --batch-size=8 --source-lang de --target-lang en --scoring sacrebleu --max-tokens 4096 --tokenizer moses --remove-bpe --model-overrides "{'load_knn_datastore': True, 'use_knn_datastore': True, 'dstore_filename': '/media/hdd1/pam/mt/data/joint_multi_domain/koran/datastore_32/pruned/pca/pruned_2_pca_256_', 'dstore_size': 353007, 'dstore_fp16': False, 'k': 8, 'probe': 32, 'faiss_metric_type': 'l2', 'use_gpu_to_search': False, 'move_dstore_to_mem': True, 'no_load_keys': True,'knn_lambda_type': 'fix', 'knn_lambda_value': .7, 'knn_temperature_type': 'fix', 'knn_temperature_value': 100, 'knn_cache': False , 'knn_cache_threshold': 6}" --quiet
 
 echo "koran pca 256 pruned 2 batch 8"
+
+
 
 
 
