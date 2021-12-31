48fc26a2313291e30967c9d30891de5187d6ceb5
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2021-12-31 17:48:42 +0000

    m

--- fairseq/modules/knn_datastore.py
+++ fairseq/modules/knn_datastore.py
@@ -281,11 +281,9 @@ class KNN_Dstore(object):
                     idx[dstore_idx[i].item()].append(i)
 
             dists = torch.zeros(dstore_idx.size(0), self.k)
-            knns = torch.zeros(dstore_idx.size(0), self.k, dtype=torch.int32)
+            knns = torch.LongTensor(torch.zeros(dstore_idx.size(0), self.k, dtype=torch.int32))
+            
             for i in idx.keys():
-                x,y = self.indexes[i].search(queries[idx[i]], self.k)
-                print(x)
-                print(y)
                 dists[idx[i]], knns[idx[i]] = self.indexes[i].search(queries[idx[i]], self.k)
 
         else:
