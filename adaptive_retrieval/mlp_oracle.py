c237b3118e9d864f94d890cc6347d7f62d3d7e22
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2022-01-05 14:02:19 +0000

    m

--- adaptive_retrieval/mlp_oracle.py
+++ adaptive_retrieval/mlp_oracle.py
@@ -212,7 +212,7 @@ class MLPOracle(nn.Module):
                 weights = torch.ones(targets.size(0)).cuda()
                 for i in range(len(targets)):
                     if targets[i]==0:
-                        weights[i]=.5
+                        weights[i]=.25
                 self.loss_ = self.loss_ = nn.BCELoss(weight=weights)
 
             loss = self.loss_(scores.squeeze(-1), targets.squeeze(-1))
