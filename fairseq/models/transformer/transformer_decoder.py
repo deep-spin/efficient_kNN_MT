eb6d0cb0e2379039144b5ab4deb0b6d439096d1d
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2022-01-07 15:31:50 +0100

    m

--- fairseq/models/transformer/transformer_decoder.py
+++ fairseq/models/transformer/transformer_decoder.py
@@ -556,10 +556,10 @@ class TransformerDecoderBase(FairseqIncrementalDecoder):
                 else:
                     scores = self.oracle_mlp.forward(last_hidden).squeeze(-1)
                 indices = (scores < 0.5).nonzero()[:,0]
-                knn_lambda = (torch.ones(last_hidden.size(0), dtype=torch.bool) * knn_lambda).unsqueeze(-1).cuda()
-                print(knn_lambda.shape)
+                #knn_lambda = (torch.ones(last_hidden.size(0), dtype=torch.bool) * knn_lambda).unsqueeze(-1).cuda()
+                #print(knn_lambda)
                 mask[indices] = False
-                knn_lambda[indices] = 0
+                #knn_lambda[indices] = 0
                 last_hidden=last_hidden[mask]
 
                 self.need_to_search += scores.size(0) - indices.size(0) 
