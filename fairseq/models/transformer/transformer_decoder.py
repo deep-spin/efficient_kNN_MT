cce12bddf774278d0bd83951cf1deaaceb01e96b
pedrohenriqueamartins <pedrohenriqueamartins@gmail.com>
2022-01-10 11:23:22 +0100

    m

--- fairseq/models/transformer/transformer_decoder.py
+++ fairseq/models/transformer/transformer_decoder.py
@@ -401,9 +401,7 @@ class TransformerDecoderBase(FairseqIncrementalDecoder):
                     network_probs = utils.softmax(self.output_layer(x), dim=-1, onnx_trace=self.onnx_trace)
                     conf=torch.max(network_probs, -1).values
                     ent=torch.distributions.Categorical(network_probs).entropy()
-                    print(conf)
-                    print(ent)
-                    knn_lambda = self.lambda_mlp.forward(last_hidden, conf, ent)
+                    knn_lambda = self.lambda_mlp.forward(last_hidden, conf=conf, ent=ent)
 
                 elif self.knn_use_conf_ent and self.knn_use_freq_fert:
                     network_probs = utils.softmax(self.output_layer(x), dim=-1, onnx_trace=self.onnx_trace)
